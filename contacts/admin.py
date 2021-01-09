d1c58819923e16cfa4e4ff8388dc2873b88d8ab6
franciscocamellon <franciscocamellon@gmail.com>
2021-01-09 14:46:14 -0300

    Minor fixes

--- contacts/admin.py
+++ contacts/admin.py
@@ -6,7 +6,7 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'last_name', 'phone',
                     'email', 'creation_date', 'category')
     list_display_links = ('id', 'name', 'last_name')
-    list_filter = ('name', 'last_name')
+    # list_filter = ('name', 'last_name')
     list_per_page = 10
     search_fields = ('name', 'last_name', 'phone')
 
