b7249ddadb728d971adead4f9a5da4705758ac58
franciscocamellon <franciscocamellon@gmail.com>
2021-01-09 20:55:40 -0300

    Update views to query objects from models

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def index(request):
-    return render(request, 'contacts/index.html')
+    _contacts = Contact.objects.all()
+    return render(request, 'contacts/index.html', {
+        'contacts': _contacts
+    })
+
+def show_contact(request, contact_id):
+    _contact = Contact.objects.get(id=contact_id)
+    return render(request, 'contacts/show_contact.html', {
+        'contact': _contact
+    })
