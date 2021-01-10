cba26489a1163c8324f11145790c6af150886fd9
franciscocamellon <franciscocamellon@gmail.com>
2021-01-09 21:21:31 -0300

    Add a boolean field to show or not a contact

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,11 +4,12 @@ from .models import Category, Contact
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'last_name', 'phone',
-                    'email', 'creation_date', 'category')
+                    'email', 'creation_date', 'category', 'show')
     list_display_links = ('id', 'name', 'last_name')
     # list_filter = ('name', 'last_name')
     list_per_page = 10
     search_fields = ('name', 'last_name', 'phone')
+    list_editable = ('phone', 'show')
 
 
 admin.site.register(Category)
