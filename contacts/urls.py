f64e85a1b1f45ed33aa418da60b2fc90aee5d49b
franciscocamellon <franciscocamellon@gmail.com>
2021-01-10 17:49:00 -0300

    Adding a search field

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
     path('<int:contact_id>', views.show_contact, name='show_contact'),
 ]
